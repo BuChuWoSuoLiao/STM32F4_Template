0d97b5c1b1d574d1159ba4819bdf10a9d1cc4053
liaomou <liaomouyo@163.com>
2023-08-11 10:38:44 +0800

    走马灯

--- LED跑马灯/HAREWARE/LED/LED.C
+++ LED跑马灯/HAREWARE/LED/LED.C
@@ -1,7 +1,16 @@
 #include "led.h"
 #include "stm32f4xx.h"
 
+/////////////////////////////////////
 // 初始化led
+// 1.初始化时钟
+// 2.初始化io
+// 	（1）定义io口
+// 	（2）io工作模式
+//	（3）io输出输入模式
+// 	（4）上拉下拉
+// 	（5）速度
+/////////////////////////////////////
 void LED_Init(void){
 		// 定义gpio初始化结构体
 	GPIO_InitTypeDef GPIO_InitStructure;
@@ -9,8 +18,8 @@ void LED_Init(void){
 	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
 	
 	// GPIOF9,F10初始化设置
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;	// 定义io口
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;						// 普通输出模式
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;		// 定义io口
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;							// 普通输出模式
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;						// 推挽输出
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;				// 100mhz速度
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;							// 上拉
